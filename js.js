0da24ba03b9a833e83dc399dd1d48425c2906c01
okaforstephanie2010-jpg <okaforstephanie2010@gmail.com>
2025-10-21 22:04:16 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+paypal.HostedButtons({
+  hostedButtonId: "BJBK48NJDK3TE", // Replace with your real button ID
+}).render("#paypal-container-BJBK48NJDK3TE");
+
+paypal.HostedButtons({
+  hostedButtonId: "XYZ123456", // Replace with your second product’s button ID
+}).render("#paypal-container-XYZ123456");
